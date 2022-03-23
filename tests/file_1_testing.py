ccfe0cd83580b2f9adb80bbd91c842dd2b7ff1ba
Petro Pistun <ppistun@n-ix.com>
2022-03-23 15:48:27 +0200

    Fixed running projects

--- tests/file_1_testing.py
+++ tests/file_1_testing.py
@@ -1,11 +1,19 @@
-import sys
-import os
-sys.path.insert(0, os.path.dirname(sys.path[0]))
+print("File:  " + __file__ + " -   testing")
 
-from matrix import Matrix 
+
+# import sys
+# import os
+# sys.path.insert(0, os.path.dirname(sys.path[0]))
+
+from lib.matrix import Matrix 
 mx = Matrix()
-a = mx.load('file_1_test.txt', ' ')
+a = mx.load('./tests/file_1_test.txt', ' ')
 if a:
     print('Loading completed')
+    print('TEST - PASSED')
 else:
     print('Loading failed')
+    print('TEST - FAILED')
+
+
+print("END TEST File:  " + __file__)
